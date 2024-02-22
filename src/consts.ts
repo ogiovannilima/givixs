5c03d57f464b69faa31ddbe3791fb55f8b09aff7
Giovanni Lima <giovannilimadev@gmail.com>
2024-02-21 22:06:53 -0300

    first day coding my personal site

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const Credits = {
+    'bg-404': 'https://unsplash.com/pt-br/fotografias/caixa-de-papelao-marrom-na-mesa-branca-wpyQ_uSsQdU?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash',
+    'main-bg': 'https://unsplash.com/pt-br/fotografias/textil-branco-com-linha-preta-xz485Eku8O4?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash'
+}
